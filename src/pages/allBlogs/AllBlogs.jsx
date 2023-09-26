5fb99fe03c656a05dbcdef416355ef87e75a0683
johnvdpk <johnvdpk@gmail.com>
2023-09-26 13:15:23 +0200

    dit is wel weer een goede opfris cursus

--- src/pages/allBlogs/AllBlogs.jsx
+++ src/pages/allBlogs/AllBlogs.jsx
@@ -1,12 +1,16 @@
 import './AllBlogs.css'
+import posts from '../../constants/data.json';
+
 
 function AllBlogs () {
 
     return (
         <>
-
+            <p>Aantal = {posts.length} Posts</p>
+            {posts.map((post) => (
+                <p key={post.id}>{post.title}</p>
+            ))}
         </>
-
     );
 }
 
--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,14 +1,36 @@
 import './Blog.css'
 import { useParams } from "react-router-dom";
+import posts from "../../constants/data.json";
+import post from "../post/Post.jsx";
 
 function Blog () {
+    const { articleNumber } = useParams();
 
-    const {articleNumber} = useParams();
+    function formatDateTime(dateTimeString) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = new Date(dateTimeString).toLocaleDateString(undefined, options);
+        return formattedDate;
+    }
+
+    // Zoek het juiste blogartikel op basis van het articleNumber
+    const selectedPost = posts.find((post) => post.id === parseInt(articleNumber));
 
     return (
         <>
             <div>Het Artikel nummer is {articleNumber}</div>
 
+            {selectedPost && (
+                <>
+                    <p key={selectedPost.id}>{formatDateTime(selectedPost.created)}</p>
+                    <p key={selectedPost.id}>{selectedPost.author}</p>
+                    <p key={selectedPost.id}>{selectedPost.title}</p>
+                    <p key={selectedPost.id}>{selectedPost.subtitle}</p>
+                    <p key={selectedPost.id}>{selectedPost.content}</p>
+                    <p key={selectedPost.id}>Aantal reacties: {selectedPost.comments}</p>
+                    <p key={selectedPost.id}>Aantal keer gedeeld: {selectedPost.shares}</p>
+
+                </>
+            )}
         </>
 
     );
